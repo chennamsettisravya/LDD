cd8f8d8bb8e8a0eadd886495886d81c2d8c90c3a
chennamsettisravya <113333282+chennamsettisravya@users.noreply.github.com>
2025-03-07 22:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,7 @@ model = download_model()
 
 
 
-working_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = f"{working_dir}/plant_disease_prediction_model.h5"
-# Load the pre-trained model
-model = tf.keras.models.load_model(model_path)
+
 
 # loading the class names
 class_indices = json.load(open(f"{working_dir}/class_indices.json"))
